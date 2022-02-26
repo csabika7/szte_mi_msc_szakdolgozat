5696c6214216158cc763c63bf1edacde0c05e011
csabika7 <kcsavatar@gmail.com>
2022-02-26 18:29:15 +0100

    ingress controller openid connect auth code flow

--- sso-server/realm-setup/add_realm.sh
+++ sso-server/realm-setup/add_realm.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env sh
 
-identity_provider_config=$(cat /realm_data/identity_provider_config.json | tr -d "\n" | tr -d " ")
-
+# Authenticate as an admin to create resources in Keycloak
 access_token=$(curl $KEYCLOAK_PROTO://$KEYCLOAK_HOST/realms/master/protocol/openid-connect/token --insecure \
   --request POST \
   --data "client_id=admin-cli" \
@@ -9,12 +8,15 @@ access_token=$(curl $KEYCLOAK_PROTO://$KEYCLOAK_HOST/realms/master/protocol/open
   --data "password=$KEYCLOAK_ADMIN_PASSWORD" \
   --data "grant_type=password" | jq --raw-output ".access_token")
 
+# Create realm
 curl $KEYCLOAK_PROTO://$KEYCLOAK_HOST/admin/realms --insecure \
      --request POST \
      --header "Content-Type: application/json" \
      --header "Authorization: Bearer  $access_token" \
-     --data "{\"realm\":\"$KEYCLOAK_REALM\"}"
+     --data "{\"realm\":\"$KEYCLOAK_REALM\",\"enabled\":true}"
 
+# Create Identity Provider
+identity_provider_config=$(cat /realm_data/identity_provider_config.json | tr -d "\n" | tr -d " ")
 curl $KEYCLOAK_PROTO://$KEYCLOAK_HOST/admin/realms/$KEYCLOAK_REALM/identity-provider/instances \
      --insecure --request POST \
      --header "Content-Type: application/json" \
