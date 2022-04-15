14e53824969c8428baafff8d024ea4d43bebb9e2
csabika7 <kcsavatar@gmail.com>
2022-04-15 15:47:04 +0200

    ui update: toast for notifying user of successful actions

--- model-admin-web/src/app/app.module.ts
+++ model-admin-web/src/app/app.module.ts
@@ -6,14 +6,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ModelManagementComponent } from './model-management/model-management.component';
 import { FileUploadModule } from "primeng/fileupload";
-import {InputTextModule} from 'primeng/inputtext';
-import {ButtonModule} from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { TagModule } from 'primeng/tag';
-import {DataViewModule} from 'primeng/dataview';
-import {DialogModule} from 'primeng/dialog';
+import { DataViewModule } from 'primeng/dataview';
+import { DialogModule } from 'primeng/dialog';
 import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-import {ProgressBarModule} from 'primeng/progressbar';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { ToastModule } from 'primeng/toast';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import {ProgressBarModule} from 'primeng/progressbar';
     DataViewModule,
     DialogModule,
     NoopAnimationsModule,
-    ProgressBarModule
+    ProgressBarModule,
+    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- model-admin-web/src/app/model-management/model-management.component.ts
+++ model-admin-web/src/app/model-management/model-management.component.ts
@@ -3,12 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Model } from './model';
 import { ModelStorageService } from '../model-storage.service';
 import { ModelOrchestrationService } from '../model-orchestration.service';
+import { MessageService } from 'primeng/api';
 
 
 @Component({
   selector: 'app-model-management',
   templateUrl: './model-management.component.html',
-  styleUrls: ['./model-management.component.css']
+  styleUrls: ['./model-management.component.css'],
+  providers: [MessageService]
 })
 export class ModelManagementComponent implements OnInit {
 
@@ -19,7 +21,7 @@ export class ModelManagementComponent implements OnInit {
   displayDialog = false;
 
   constructor(private http: HttpClient, private modelStorageService: ModelStorageService,
-    private modelOrchestrationService: ModelOrchestrationService) {
+    private modelOrchestrationService: ModelOrchestrationService, private msgService: MessageService) {
       this.models = [];
   }
 
@@ -37,24 +39,28 @@ export class ModelManagementComponent implements OnInit {
       this.displayDialog = false;
       // workaround unUpload does not work
       this.updateModelList();
+      this.msgService.add({severity: "success", summary: `New model has been uploaded.`, detail: ""});
     }
   }
 
   deleteModel(selectedModel: Model) {
     this.modelStorageService.deleteModel(selectedModel.id).subscribe((data: any) => {
       this.models = this.models.filter(model => model.id != selectedModel.id)
+      this.msgService.add({severity: "success", summary: `${selectedModel.id} has been deleted.`, detail: ""});
     });
   }
 
   activateModel(selectedModel: Model) {
     this.modelOrchestrationService.activateModel(selectedModel.id).subscribe((data: any) => {
       selectedModel.active = true;
+      this.msgService.add({severity: "success", summary: `${selectedModel.id} has been activated.`, detail: ""});
     });
   }
 
   deactivateModel(selectedModel: Model) {
     this.modelOrchestrationService.deactivateModel(selectedModel.id).subscribe((data: any) => {
       selectedModel.active = false;
+      this.msgService.add({severity: "success", summary: `${selectedModel.id} has been deactivated.`, detail: ""});
     });
   }
 
