b0d441dad4af660bfa473c0f120ec28b9fabe628
csabika7 <kcsavatar@gmail.com>
2022-05-14 18:11:20 +0200

    vgg16 model with transfer learning

--- models/ambrosia_artemisiifolia/training.py
+++ models/ambrosia_artemisiifolia/training.py
@@ -1,16 +1,17 @@
+from pathlib import Path
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import Flatten, BatchNormalization, Dropout
 from keras.layers.convolutional import Conv2D, MaxPooling2D
 from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.vgg16 import VGG16
 import os
 
 
 # Define model
-def define_model():
-    # AlexNet
+def alex_net():
     m = Sequential()
-    m.add(Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4), activation='relu', input_shape=(227, 227, 3)))
+    m.add(Conv2D(filters=96, kernel_size=(11, 11), strides=(4, 4), activation='relu', input_shape=(227, 227, 1)))
     m.add(BatchNormalization())
     m.add(MaxPooling2D(pool_size=(3, 3), strides=(2, 2)))
     m.add(Conv2D(filters=256, kernel_size=(5, 5), strides=(1, 1), activation='relu'))
@@ -30,10 +31,34 @@ def define_model():
     return m
 
 
+def vgg16():
+    base_model = VGG16(
+        include_top=False,
+        weights="imagenet",
+        input_tensor=None,
+        input_shape=(227, 227, 3),
+        pooling="max",
+        classes=2
+    )
+    base_model.trainable = False
+
+    m = Sequential([
+        base_model,
+        Flatten(),
+        Dense(512, activation='relu'),
+        Dropout(0.5),
+        Dense(512, activation='relu'),
+        Dense(1, activation='sigmoid')
+    ])
+
+    m.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return m
+
+
 # Load data
 
-TRAINING_DIR = os.path.join(os.getcwd(), "training_db", "preprocessed", "train")
-VALIDATION_DIR = os.path.join(os.getcwd(), "training_db", "preprocessed", "validation")
+TRAINING_DIR = os.path.join(Path(os.getcwd()).parent.parent, "training_db", "preprocessed", "train")
+VALIDATION_DIR = os.path.join(Path(os.getcwd()).parent.parent, "training_db", "preprocessed", "validation")
 BATCH_SIZE = 8
 generator = ImageDataGenerator()
 
@@ -51,10 +76,11 @@ validation = generator.flow_from_directory(
     batch_size=BATCH_SIZE
 )
 
-model = define_model()
+# model = alex_net()
+model = vgg16()
 
 # training
-model.fit(train, steps_per_epoch=296/BATCH_SIZE, epochs=10, validation_data=validation, validation_steps=72/BATCH_SIZE)
+model.fit(train, steps_per_epoch=451/BATCH_SIZE, epochs=10, validation_data=validation, validation_steps=112/BATCH_SIZE)
 scores = model.evaluate(validation, verbose=0)
 
 # evaluation
